a62b741682a957e0abb7588aa082c46a6900ca92
IsoBones <isoboness@gmail.com>
2026-05-01 21:02:51 +1000

    Upload ADF_Core

---
+++ Addons/ADF_Core/config.cpp
@@ -0,0 +1,21 @@
+class CfgPatches
+{
+	class ADF_Core
+	{
+		requiredAddons[] = {"A3_Data_F","cba_jr"};
+		requiredVersion = 0.1;
+		units[] = {};
+		weapons[] = {};
+	};
+};
+/* Definitions & Inheritance */
+#include "basicDefines_A3.hpp"
+#include "cfgFactionClasses.hpp"
+#include "cfgEditorSubcategories.hpp"
+#include "cfgMarkers.hpp"
+#include "cfgMovesBasic.hpp"
+#include "cfgMovesMaleSdr.hpp"
+#include "cfgUnitInsignia.hpp"
+#include "cfgVehicleClasses.hpp"
+#include "cfgVehicles.hpp"
+#include "sensors.hpp"
