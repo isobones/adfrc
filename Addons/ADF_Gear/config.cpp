0b4ed8684a74d5b273bb95ca0e22111954b03217
IsoBones <isoboness@gmail.com>
2026-05-02 09:42:46 +1000

    Create config.cpp

---
+++ Addons/ADF_Gear/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches
+{
+	class ADF_Gear
+	{
+		name="ADF_Gear";
+		author=$STR_ADF_AUTHOR;
+		requiredAddons[]=
+        	{
+			"A3_Data_F",
+			"ADF_Core"
+		};
+		units[]={};
+		weapons[]={};
+		fileName = "ADF_Gear.pbo";
+	};
+};
