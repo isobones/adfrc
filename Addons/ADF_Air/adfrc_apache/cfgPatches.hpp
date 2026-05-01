88f19d3f6d2f0da170683afb7dc0999d03c0653c
IsoBones <isoboness@gmail.com>
2026-05-01 21:02:06 +1000

    Upload ADF_Air

---
+++ Addons/ADF_Air/adfrc_apache/cfgPatches.hpp
@@ -0,0 +1,18 @@
+class CfgPatches
+{
+	class adfrc_apache
+	{
+		requiredAddons[] = 
+		{
+			"A3_Weapons_F",
+			"adfrc_air_c130",
+			"adfrc_faction"
+		};
+		requiredVersion = 0.1;
+		units[] =
+        {
+			"adfrc_apache"
+        };
+		weapons[] = {/* Automated */};
+	};
+};
---
+++ Addons/ADF_Air/adfrc_apache/turrets.hpp
@@ -0,0 +1,244 @@
+class Turrets: Turrets
+{
+    class MainTurret: MainTurret
+    {
+        turretInfoType = RscOptics_Heli_Attack_01_gunner;
+        commanding = -1;
+        isCopilot = true;
+        CanEject = false;
+        startEngine = false;
+        primaryGunner = true;
+
+        /* Servos */
+        minElev = -60;
+        maxElev = 10;
+        initElev = 0;
+        minTurn = -90;
+        maxTurn = 90;
+        initTurn = 0;
+        maxHorizontalRotSpeed = 1.8;
+        maxVerticalRotSpeed = 1.5;
+        stabilizedInAxes = "StabilizedInAxesBoth";
+        canLock = 1;
+
+        /* Sounds */
+        soundServo[] = {"\A3\Sounds_F\vehicles\armor\noises\servo_armor_gunner", db-5, 1, 30};
+        soundServoVertical[] = {"\A3\Sounds_F\vehicles\armor\noises\servo_armor_gunner_vertical", db-5, 1, 30};
+        soundLocked[] = {"\A3\Sounds_F\weapons\Rockets\opfor_lock_1", db+10, 1};
+        soundIncommingMissile[] = {"\A3\Sounds_F\weapons\Rockets\locked_3", db+10, 1};
+        soundEngine[] = {"\A3\Sounds_F\vehicles\armor\noises\servo_armor_turret", db-5, 1, 30};
+
+        /* Optics */
+        gunnerOpticsModel = "\A3\Weapons_F\Reticle\optics_empty.p3d";
+        gunnerOpticsEffect[] = {"TankCommanderOptics1", "BWTV"};
+        gunnerForceOptics = false;
+        class OpticsIn
+        {
+            class Wide
+            {
+                opticsDisplayName = "WFOV";
+                directionStabilized = true;
+                initAngleX = 0;
+                initAngleY = 0;
+                minAngleX = -60;
+                maxAngleX = 15;
+                minAngleY = -90;
+                maxAngleY = 90;
+                initFov = 0.466;
+                minFov = 0.466;
+                maxFov = 0.466;
+                visionMode[] = {"Normal", "NVG", "Ti"};
+                thermalMode[] = {0, 1};
+                gunnerOpticsModel = "\A3\Weapons_F_Beta\Reticle\Heli_Attack_01_Optics_Gunner_wide_F.p3d";
+            };
+            class Medium: Wide
+            {
+                opticsDisplayName = "MFOV";
+                initFov = 0.093;
+                minFov = 0.093;
+                maxFov = 0.093;
+                gunnerOpticsModel = "\A3\Weapons_F_Beta\Reticle\Heli_Attack_01_Optics_Gunner_medium_F.p3d";
+            };
+            class Narrow: Wide
+            {
+                opticsDisplayName = "NFOV";
+                initFov = 0.029;
+                minFov = 0.029;
+                maxFov = 0.029;
+                gunnerOpticsModel = "\A3\Weapons_F_Beta\Reticle\Heli_Attack_01_Optics_Gunner_narrow_F.p3d";
+            };
+        };
+        class OpticsOut
+        {
+            class Monocular
+            {
+                initFov = 0.9;
+                minFov = 0.25;
+                maxFov = 1.25;
+                minAngleX = -65;
+                maxAngleX = 85;
+                minAngleY = -150;
+                maxAngleY = 150;
+                minMoveX = -0.2;
+                maxMoveX = 0.2;
+                minMoveY = -0.1;
+                maxMoveY = 0.1;
+                minMoveZ = -0.1;
+                maxMoveZ = 0.2;
+                initAngleX = 0;
+                initAngleY = 0;
+                visionMode[] = {"Normal", "NVG"};
+                gunnerOpticsModel = "";
+                gunnerOpticsEffect[] = {};
+            };
+        };
+
+        /* Sensors & Components */
+        showAllTargets = 2;
+
+        class Components
+        {
+            class VehicleSystemsDisplayManagerComponentLeft: DefaultVehicleSystemsDisplayManagerLeft
+            {
+                class Components: components
+                {
+                    class EmptyDisplay
+                    {
+                        componentType = "EmptyDisplayComponent";
+                    };
+                    class MinimapDisplay
+                    {
+                        componentType = "MinimapDisplayComponent";
+                        resource = "RscCustomInfoAirborneMiniMap";
+                    };
+                    class CrewDisplay
+                    {
+                        componentType = "CrewDisplayComponent";
+                        resource = "RscCustomInfoCrew";
+                    };
+                    class UAVDisplay
+                    {
+                        componentType = "UAVFeedDisplayComponent";
+                    };
+                    class VehiclePrimaryGunnerDisplay
+                    {
+                        componentType = "TransportFeedDisplayComponent";
+                        source = "PrimaryGunner";
+                    };
+                    class VehicleMissileDisplay
+                    {
+                        componentType = "TransportFeedDisplayComponent";
+                        source = "Missile";
+                    };
+                    class SensorDisplay
+                    {
+                        componentType = "SensorsDisplayComponent";
+                        range[] = {4000, 2000, 16000, 8000};
+                        resource = "RscCustomInfoSensors";
+                    };
+                };
+            };
+            class VehicleSystemsDisplayManagerComponentRight: DefaultVehicleSystemsDisplayManagerRight
+            {
+                defaultDisplay = "SensorDisplay";
+                class Components: components
+                {
+                    class EmptyDisplay
+                    {
+                        componentType = "EmptyDisplayComponent";
+                    };
+                    class MinimapDisplay
+                    {
+                        componentType = "MinimapDisplayComponent";
+                        resource = "RscCustomInfoAirborneMiniMap";
+                    };
+                    class CrewDisplay
+                    {
+                        componentType = "CrewDisplayComponent";
+                        resource = "RscCustomInfoCrew";
+                    };
+                    class UAVDisplay
+                    {
+                        componentType = "UAVFeedDisplayComponent";
+                    };
+                    class VehiclePrimaryGunnerDisplay
+                    {
+                        componentType = "TransportFeedDisplayComponent";
+                        source = "PrimaryGunner";
+                    };
+                    class VehicleMissileDisplay
+                    {
+                        componentType = "TransportFeedDisplayComponent";
+                        source = "Missile";
+                    };
+                    class SensorDisplay
+                    {
+                        componentType = "SensorsDisplayComponent";
+                        range[] = {4000, 2000, 16000, 8000};
+                        resource = "RscCustomInfoSensors";
+                    };
+                };
+            };
+        };
+
+        /* MFD */
+        #include "mfd_gunner.hpp"
+
+        /* Weapons & Ammunition */
+        gunBeg = "usti hlavne";
+        gunEnd = "konec hlavne";
+        weapons[] = {"ADFRC_Chaingun_30mm", "Laserdesignator_mounted"};
+        magazines[] = {"ADFRC_1200Rnd_Chaingun_30mm", "Laserbatteries"};
+
+        /* FCS */
+        discreteDistance[] = {100, 200, 300, 400, 500, 600, 700, 800, 1000, 1200, 1500, 1800, 2100, 2500};
+        discreteDistanceInitIndex = 5;
+
+        /* Crew */
+        castGunnerShadow = true;
+        viewGunnerShadow = true;
+        outGunnerMayFire = true;
+        gunnerAction = "Heli_Attack_03_Gunner";
+        gunnerInAction = "Heli_Attack_03_Gunner";
+        gunnerLeftHandAnimName = "";
+        gunnerRightHandAnimName = "";
+        gunnerLeftLegAnimName = "";
+        gunnerRightLegAnimName = "";
+
+        /* Enter & Exit Animations */
+        preciseGetInOut = false;
+        getInRadius = 1.5;
+        gunnerGetInAction = "GetInHigh";
+        gunnerGetOutAction = "GetOutHigh";
+
+        /* Damage */
+        class HitPoints
+        {
+            class HitTurret
+            {
+                armor = 1;
+                material = -1;
+                name = "main_turret_hit";
+                visual = "main_trav";
+                passThrough = 0.3;
+                radius = 0.2;
+            };
+            class HitGun
+            {
+                armor = 1;
+                material = -1;
+                name = "main_gun_hit";
+                visual = "main_elev";
+                passThrough = 0.3;
+                radius = 0.2;
+            };
+        };
+
+        /* Selections & Memory Points */
+        selectionFireAnim = "zasleh";
+        memoryPointGunnerOptics = "gunnerview";
+        memoryPointGun = "machinegun";
+        memoryPointsGetInGunner = "pos gunner";
+        memoryPointsGetInGunnerDir = "pos gunner dir";
+    };
+};
