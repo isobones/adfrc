aef0d783037d36ee14633568d230743291a47d8e
IsoBones <isoboness@gmail.com>
2026-05-02 10:56:17 +1000

    upload adfrc_ef88

---
+++ Addons/ADF_Weapons/adfrc_ef88/presets.hpp
@@ -0,0 +1,212 @@
+////EF88
+class ADFRC_Preset_EF88_Black_Spectr_Squad: ADFRC_EF88_Black
+{
+	author = "Brucey";
+	class LinkedItems
+	{
+		class LinkedItemsOptic
+		{
+			slot = CowsSlot;
+			item = "ADFRC_Specter_RAR";
+		};
+		class LinkedItemsAcc
+		{
+			slot = PointerSlot;
+			item = "ADFRC_L3Squad_EF88_Top_IRL";
+		};
+		class LinkItemsUnder
+		{
+			slot = "UnderBarrelSlot";
+			item = "ADFRC_Grippod";
+		};
+	};
+};
+
+class ADFRC_Preset_EF88_Black_Spectr_WMLX: ADFRC_EF88_Black
+{
+	author = "Brucey";
+	class LinkedItems
+	{
+		class LinkedItemsOptic
+		{
+			slot = CowsSlot;
+			item = "ADFRC_Specter_RAR";
+		};
+		class LinkedItemsAcc
+		{
+			slot = PointerSlot;
+			item = "ADFRC_WMLx_BLK_R_WL";
+		};
+		class LinkItemsUnder
+		{
+			slot = "UnderBarrelSlot";
+			item = "ADFRC_Grippod";
+		};
+	};
+};
+
+class ADFRC_Preset_EF88_Black_Spectr: ADFRC_EF88_Black
+{
+	author = "Brucey";
+	class LinkedItems
+	{
+		class LinkedItemsOptic
+		{
+			slot = CowsSlot;
+			item = "ADFRC_Specter_RAR";
+		};
+		class LinkItemsUnder
+		{
+			slot = "UnderBarrelSlot";
+			item = "ADFRC_Grippod";
+		};
+	};
+};
+
+class ADFRC_Preset_EF88_Camo_Spectr_Squad: ADFRC_EF88_Camo
+{
+	author = "Brucey";
+	class LinkedItems
+	{
+		class LinkedItemsOptic
+		{
+			slot = CowsSlot;
+			item = "ADFRC_Specter_RAR_C_KF";
+		};
+		class LinkedItemsAcc
+		{
+			slot = PointerSlot;
+			item = "ADFRC_L3Squad_EF88_Top_IRL";
+		};
+		class LinkItemsUnder
+		{
+			slot = "UnderBarrelSlot";
+			item = "ADFRC_Grippod";
+		};
+	};
+};
+
+class ADFRC_Preset_EF88_Camo_Spectr_PEQ15: ADFRC_EF88_Camo
+{
+	author = "Brucey";
+	class LinkedItems
+	{
+		class LinkedItemsOptic
+		{
+			slot = CowsSlot;
+			item = "ADFRC_Specter_C_RAR";
+		};
+		class LinkedItemsAcc
+		{
+			slot = PointerSlot;
+			item = "ADFRC_PEQ15_BLK_IRL";
+		};
+		class LinkItemsUnder
+		{
+			slot = "UnderBarrelSlot";
+			item = "ADFRC_Grippod";
+		};
+	};
+};
+
+///EF88C
+class ADFRC_Preset_EF88C_Black_Spectr: ADFRC_EF88C_Black
+{
+	author = "Brucey";
+	class LinkedItems
+	{
+		class LinkedItemsOptic
+		{
+			slot = CowsSlot;
+			item = "ADFRC_Specter_RAR";
+		};
+		class LinkItemsUnder
+		{
+			slot = "UnderBarrelSlot";
+			item = "ADFRC_Grippod";
+		};
+	};
+};
+
+class ADFRC_Preset_EF88C_Black_Spectr_Squad: ADFRC_EF88C_Black
+{
+	author = "Brucey";
+	class LinkedItems
+	{
+		class LinkedItemsOptic
+		{
+			slot = CowsSlot;
+			item = "ADFRC_Specter_RAR";
+		};
+		class LinkedItemsAcc
+		{
+			slot = PointerSlot;
+			item = "ADFRC_L3Squad_IRL";
+		};
+		class LinkItemsUnder
+		{
+			slot = "UnderBarrelSlot";
+			item = "ADFRC_Grippod";
+		};
+	};
+};
+
+class ADFRC_Preset_EF88C_Camo_Spectr: ADFRC_EF88C_Camo
+{
+	author = "Brucey";
+	class LinkedItems
+	{
+		class LinkedItemsOptic
+		{
+			slot = CowsSlot;
+			item = "ADFRC_Specter_C_RAR";
+		};
+		class LinkItemsUnder
+		{
+			slot = "UnderBarrelSlot";
+			item = "ADFRC_Grippod";
+		};
+	};
+};
+
+///SL40
+class ADFRC_Preset_EF88_SL40_Camo_Spectr: ADFRC_EF88_SL40_Camo
+{
+	author = "Brucey";
+	class LinkedItems
+	{
+		class LinkedItemsOptic
+		{
+			slot = CowsSlot;
+			item = "ADFRC_Specter_RAR_C_KF";
+		};
+		class LinkItemsUnder
+		{
+			slot = "UnderBarrelSlot";
+			item = "ADFRC_Grippod";
+		};
+	};
+};
+
+class ADFRC_Preset_EF88_SL40_Camo_Spectr_Squad: ADFRC_EF88_SL40_Camo
+{
+	author = "Brucey";
+	class LinkedItems
+	{
+		class LinkedItemsOptic
+		{
+			slot = CowsSlot;
+			item = "ADFRC_Specter_RAR_C_KF";
+		};
+		class LinkedItemsAcc
+		{
+			slot = PointerSlot;
+			item = "ADFRC_L3Squad_IRL";
+		};
+		class LinkItemsUnder
+		{
+			slot = "UnderBarrelSlot";
+			item = "ADFRC_Grippod";
+		};
+	};
+};
