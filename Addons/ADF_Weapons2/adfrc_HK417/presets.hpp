e42bd8bf50045721df2426351d9747600ca18d8d
IsoBones <isoboness@gmail.com>
2026-05-02 10:45:25 +1000

    upload ADF_Weapons2

---
+++ Addons/ADF_Weapons2/adfrc_HK417/presets.hpp
@@ -0,0 +1,25 @@
+
+//Eotech WL
+class ADFRC_HK417_Black_DMR: ADFRC_HK417_Black
+{
+	author = "Brucey";
+	scope = 1;
+	class LinkedItems
+	{
+		class LinkedItemsOptic
+			{
+				slot="CowsSlot";
+				item="ADFRC_acog_ta648_rds";
+			};
+			class LinkedItemsPointer
+			{
+				slot="PointerSlot";
+				item="acc_pointer_IR";
+			};
+			class LinkItemsUnder
+			{
+				slot = "UnderBarrelSlot";
+				item = "ADFRC_Atlas_BT10";
+			};
+	};
+};
