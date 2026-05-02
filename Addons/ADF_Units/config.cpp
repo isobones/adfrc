ce827804447e84ffbeb1fb88c7e7095fdeaa0a9e
IsoBones <isoboness@gmail.com>
2026-05-02 10:12:29 +1000

    upload ADF_Units

---
+++ Addons/ADF_Units/config.cpp
@@ -0,0 +1,35 @@
+class CfgPatches{
+	class ADF_Units{
+		name="ADF_Units";
+		author="ADFRC Team";
+		requiredAddons[]={
+			"A3_Data_F",
+			"ADF_Core",
+			"ADF_Gear"
+		};
+		units[]={};
+		weapons[]={};
+		//fileName = "ADF_Units.pbo";
+	};
+};
+
+class CfgVehicles{
+	#include "Modern\RAR\Infantry.hpp"
+	#include "Modern\RAR\Recce.hpp"
+	#include "Modern\CDO\Infantry.hpp"
+	//#include "Modern\CDO\TAG.hpp"   ## BASE CLASSES DONT EVEN EXIST FOR THESE YET!
+	#include "Modern\SASR\Infantry.hpp"
+	//#include "Modern\SASR\TAG.hpp"  ## BASE CLASSES DONT EVEN EXIST FOR THESE YET!
+	#include "Modern\Backpacks.hpp"
+};
+
+class cfgGroups
+{  
+  class WEST
+  {
+    name="BLUFOR";
+	#include "Modern\RAR\Groups.hpp"
+	#include "Modern\CDO\Groups.hpp"
+	#include "Modern\SASR\Groups.hpp"
+};
+};
